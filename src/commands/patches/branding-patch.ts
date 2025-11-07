c8199feed3502923b7e874db1b7e8ef55bf7e24d
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-11-07 16:37:30 -0500

    Update branding to match Sulfur

--- src/commands/patches/branding-patch.ts
+++ src/commands/patches/branding-patch.ts
@@ -301,9 +301,9 @@ function configureBrandingNsis(
 !define BrandFullNameInternal "${brandingConfig.brandFullName}"
 !define BrandFullName         "${brandingConfig.brandFullName}"
 !define CompanyName           "${brandingConfig.brandingVendor}"
-!define URLInfoAbout          "https://zen-browser.app"
-!define URLUpdateInfo         "https://zen-browser.app/release-notes/\${AppVersion}"
-!define HelpLink              "https://github.com/zen-browser/desktop/issues"
+!define URLInfoAbout          "https://sulfur-browser.github.io/"
+!define URLUpdateInfo         "https://sulfur-browser.github.io/release-notes/\${AppVersion}"
+!define HelpLink              "https://github.com/sulfur-browser/desktop/issues"
 
 ; The OFFICIAL define is a workaround to support different urls for Release and
 ; Stable since they share the same branding when building with other branches that
@@ -312,7 +312,7 @@ function configureBrandingNsis(
 !define URLStubDownloadX86 "https://download.mozilla.org/?os=win&lang=\${AB_CD}&product=firefox-latest"
 !define URLStubDownloadAMD64 "https://download.mozilla.org/?os=win64&lang=\${AB_CD}&product=firefox-latest"
 !define URLStubDownloadAArch64 "https://download.mozilla.org/?os=win64-aarch64&lang=\${AB_CD}&product=firefox-latest"
-!define URLManualDownload "https://zen-browser.app/download"
+!define URLManualDownload "https://sulfur-browser.github.io/download"
 !define URLSystemRequirements "https://www.mozilla.org/firefox/system-requirements/"
 !define Channel "stable"
 
@@ -415,9 +415,9 @@ function configureProfileBranding(
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("startup.homepage_override_url", "https://zen-browser.app/whatsnew?v=%VERSION%");
-pref("startup.homepage_welcome_url", "https://zen-browser.app/welcome/");
-pref("startup.homepage_welcome_url.additional", "https://zen-browser.app/privacy-policy/");
+pref("startup.homepage_override_url", "https://sulfur-browser.github.io/whatsnew?v=%VERSION%");
+pref("startup.homepage_welcome_url", "https://sulfur-browser.github.io/welcome/");
+pref("startup.homepage_welcome_url.additional", "https://sulfur-browser.github.io/privacy-policy/");
 
 // Give the user x seconds to react before showing the big UI. default=192 hours
 pref("app.update.promptWaitTime", 691200);
@@ -426,11 +426,11 @@ pref("app.update.promptWaitTime", 691200);
 // app.update.url.details: a default value for the "More information about this
 // update" link supplied in the "An update is available" page of the update
 // wizard.
-pref("app.update.url.manual", "https://zen-browser.app/download/");
-pref("app.update.url.details", "https://zen-browser.app/release-notes/latest/");
-pref("app.releaseNotesURL", "https://zen-browser.app/whatsnew/");
-pref("app.releaseNotesURL.aboutDialog", "https://www.zen-browser.app/release-notes/%VERSION%/");
-pref("app.releaseNotesURL.prompt", "https://zen-browser.app/release-notes/%VERSION%/");
+pref("app.update.url.manual", "https://sulfur-browser.github.io/download/");
+pref("app.update.url.details", "https://sulfur-browser.github.io/release-notes/latest/");
+pref("app.releaseNotesURL", "https://sulfur-browser.github.io/whatsnew/");
+pref("app.releaseNotesURL.aboutDialog", "https://sulfur-browser.github.io/release-notes/%VERSION%/");
+pref("app.releaseNotesURL.prompt", "https://sulfur-browser.github.io/release-notes/%VERSION%/");
 
 // Number of usages of the web console.
 // If this is less than 5, then pasting code into the web console is disabled
